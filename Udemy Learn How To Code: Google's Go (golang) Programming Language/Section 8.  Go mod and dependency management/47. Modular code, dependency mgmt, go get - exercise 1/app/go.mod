abfe7cb4acfa96b2b4f91fdf11464f0c027bd1f2
Nols Smit <nols.smit@gmail.com>
2023-10-06 10:16:46 +0200

    drop by drop, the buckey gets filled

--- Udemy Learn How To Code: Google's Go (golang) Programming Language/Section 8.  Go mod and dependency management/47. Modular code, dependency mgmt, go get - exercise 1/app/go.mod
+++ Udemy Learn How To Code: Google's Go (golang) Programming Language/Section 8.  Go mod and dependency management/47. Modular code, dependency mgmt, go get - exercise 1/app/go.mod
@@ -2,4 +2,6 @@ module mainmodule
 
 go 1.21.0
 
-require github.com/nolssmit/puppy v0.0.0-20230831080724-05b39fed4f6a
+require github.com/nolssmit/puppy v0.0.0-20230908130124-6b8dc9596e9b
+
+require github.com/nolssmit/dog v0.0.0-20230904132008-953c5292569c // indirect
--- Udemy Learn How To Code: Google's Go (golang) Programming Language/Section 8.  Go mod and dependency management/47. Modular code, dependency mgmt, go get - exercise 1/app/main.go
+++ Udemy Learn How To Code: Google's Go (golang) Programming Language/Section 8.  Go mod and dependency management/47. Modular code, dependency mgmt, go get - exercise 1/app/main.go
@@ -7,12 +7,19 @@ import (
 )
 
 func main() {
-	s1 := puppy.Back()
+	s1 := puppy.Bark()
 	s2 := puppy.Barks()
 	fmt.Println(s1)
 	fmt.Println(s2)
 
+	s3 := puppy.BigBark()
+	s4 := puppy.BigBarks()
+	fmt.Println(s3)
+	fmt.Println(s4)	
+
 	// also like this
-	fmt.Println(puppy.Back())
+	fmt.Println(puppy.Bark())
 	fmt.Println(puppy.Barks())
+	fmt.Println(puppy.BigBark())
+	fmt.Println(puppy.BigBarks())
 }
