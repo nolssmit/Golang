456969ba09108d4723d3dd781013901650726691
Nols Smit <nols.smit@gmail.com>
2023-10-10 16:52:42 +0200

    WIP

---
+++ Udemy Learn How To Code: Google's Go (golang) Programming Language/Section 8.  Go mod and dependency management/52. Specifying depency version/app/main.go
@@ -0,0 +1,28 @@
+package main
+
+import (
+	"fmt"
+
+	"github.com/nolssmit/puppy"
+)
+
+func main() {
+//	puppy.From13()
+	puppy.From11()
+
+	s1 := puppy.Bark()
+	s2 := puppy.Barks()
+	fmt.Println(s1)
+	fmt.Println(s2)
+
+	s3 := puppy.BigBark()
+	s4 := puppy.BigBarks()
+	fmt.Println(s3)
+	fmt.Println(s4)	
+
+	// also like this
+	fmt.Println(puppy.Bark())
+	fmt.Println(puppy.Barks())
+	fmt.Println(puppy.BigBark())
+	fmt.Println(puppy.BigBarks())
+}
