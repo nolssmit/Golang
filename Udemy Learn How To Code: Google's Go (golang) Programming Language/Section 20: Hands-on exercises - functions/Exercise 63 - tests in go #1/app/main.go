b26caadbd17a3208b69fbe5356e267617b9669c5
Nols Smit <nols.smit@gmail.com>
2024-02-07 09:47:08 +0200

    Updated xUbuntu_Setup.odt

--- Udemy Learn How To Code: Google's Go (golang) Programming Language/Section 20: Hands-on exercises - functions/Exercise 63 - tests in go #1/app/main.go
+++ Udemy Learn How To Code: Google's Go (golang) Programming Language/Section 20: Hands-on exercises - functions/Exercise 63 - tests in go #1/app/main.go
@@ -2,8 +2,16 @@ package main
 
 import (
 	"fmt"
+	"testing"
 )
 
 func main() {
 	fmt.Println("Hello World!")
+}
+
+func TestAdd(*testing.T){
+	total := Add(5,5)
+	if total != 10 {
+		t.Errorf("Sum was incorrect")
+	}
 }
