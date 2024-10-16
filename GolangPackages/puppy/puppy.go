256586a6ecd579276ac08e7971e003e1921c89ef
Nols Smit <nols.smit@gmail.com>
2024-10-16 15:04:42 +0200

    Puppy version 1.0.0

--- GolangPackages/puppy/puppy.go
+++ GolangPackages/puppy/puppy.go
@@ -1,9 +1,21 @@
 package puppy
 
+import (
+	"github.com/nolssmit/Golang/GolangPackages/dog"
+)
+
 func Bark() string {
 	return "Woof!"
 }
 
 func Barks() string {
 	return "Woof! Woof! Woof!"
+}
+
+func BigBark() string {
+	return dog.WhenGrounUp(Bark())
+}
+
+func BigBarks() string {
+	return dog.WhenGrounUp(Barks())
 }
