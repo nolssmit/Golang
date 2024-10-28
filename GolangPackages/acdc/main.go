c8a33912ff29356603688cfedbd1c627a5c095dc
Nols Smit <nols.smit@gmail.com>
2024-10-28 17:58:38 +0200

    Documented package acdc

--- GolangPackages/acdc/main.go
+++ GolangPackages/acdc/main.go
@@ -1,5 +1,7 @@
+// Package acdc asks if you are ready to rock
 package acdc
 
+// Sum adds an unlimited number of of type int
 func Sum(x1 ... int) int {
 	s := 0
 	for _, v := range x1 {
--- Udemy Ultimate Comprehensive Course/Section 34. Testing & benchmarking/248. Example tests & documentation/app/go.mod
+++ Udemy Ultimate Comprehensive Course/Section 34. Testing & benchmarking/248. Example tests & documentation/app/go.mod
@@ -1,3 +1,5 @@
 module myapp
 
 go 1.23.1
+
+require github.com/nolssmit/Golang/GolangPackages/acdc v0.0.0-20241028143057-ef2b45c98e6a // indirect
