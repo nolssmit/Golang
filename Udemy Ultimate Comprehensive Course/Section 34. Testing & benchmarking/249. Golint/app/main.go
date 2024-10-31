e29b80d6365259423477d33b290334dcaff88cdb
Nols Smit <nols.smit@gmail.com>
2024-10-31 09:57:19 +0200

    Add package saying

--- Udemy Ultimate Comprehensive Course/Section 34. Testing & benchmarking/249. Golint/app/main.go
+++ Udemy Ultimate Comprehensive Course/Section 34. Testing & benchmarking/249. Golint/app/main.go
@@ -7,5 +7,7 @@ import (
 func main() {
 	fmt.Println("Hello, World!")
 }
+
 // To install golint:  $ go get -u golang.org/x/lint/golint
 // golint is depricated
+// Also check https://sparkbox.com/foundry/go_vet_gofmt_golint_to_code_check_in_Go
