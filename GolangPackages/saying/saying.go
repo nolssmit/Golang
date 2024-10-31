86a5a1c588b3bd605e5cc77e518fd493958f42a1
Nols Smit <nols.smit@gmail.com>
2024-10-31 12:31:14 +0200

    Added benchmarking and testing

--- GolangPackages/saying/saying.go
+++ GolangPackages/saying/saying.go
@@ -1,7 +1,31 @@
+// Package saying provides a function to greet people
 package saying
 
-import "fmt"
+import (
+	"fmt"
+	"testing"
+)
 
+// Greet returns a greeting to the user
 func Greet(s string) string {	
 	return fmt.Sprint("Welcome my dear ", s)
+}
+
+func TestGreet(t *testing.T) {	
+	s := Greet("Nols Smit")
+
+	if s != "Welcome my dear Nols Smit" {
+		t.Error("got", s, "expected","Welcome my dear Nols Smit")
+	}
+}
+
+func ExampleGreet() {	
+	fmt.Println(Greet("Nols Smit"))
+	// Output: Welcome my dear Nols Smit
+}
+
+func BenchmarkGreet(b *testing.B) {	
+	for i := 0; i < b.N; i++ {
+		Greet("Nols Smit")
+	}
 }
--- Udemy Ultimate Comprehensive Course/Section 34. Testing & benchmarking/250. Benchmark/app/main.go
+++ Udemy Ultimate Comprehensive Course/Section 34. Testing & benchmarking/250. Benchmark/app/main.go
@@ -2,15 +2,9 @@ package main
 
 import (
 	"fmt"
-	"net/http"
-	"time"
+	"github.com/nolssmit/Golang/GolangPackages/saying"
 )
 
-func greet(w http.ResponseWriter, r *http.Request) {
-	fmt.Fprintf(w, "Hello World! %s", time.Now())
-}
-
 func main() {
-	http.HandleFunc("/", greet)
-	http.ListenAndServe(":8080", nil)
+	fmt.Println(saying.Greet("Nols Smit"))
 }
