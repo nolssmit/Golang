ffe3663c9b868330afe1ea3206ed9fbf53848adb
Nols Smit <nols.smit@gmail.com>
2024-11-02 15:15:15 +0200

    WIP

--- GolangPackages/dog/dog.go
+++ GolangPackages/dog/dog.go
@@ -9,13 +9,13 @@ import (
 func WhenGrownUp(s string) string {
 	return "When the puppy grows up it says: " + strings.ToUpper(s)
 }
-// Years returns the passed in number of years in dog years.
+// Years returns the passed in number of years in dog years
 // where one human year is 10 dog years
 func Years(y int) int {
 	return  y * 10
 }	
 
-// YearsTwo returns the passed in number of years in dog years.
+// YearsTwo returns the passed in number of years in dog years
 // where one human year is 20 dog years
 func YearsTwo(y int) int {
 	return  y * 20
--- Udemy Ultimate Comprehensive Course/Section 35. Hands-on exercises - Testing & Benchmarking/Exercise 254. Hands-on exercise#1/app/go.mod
+++ Udemy Ultimate Comprehensive Course/Section 35. Hands-on exercises - Testing & Benchmarking/Exercise 254. Hands-on exercise#1/app/go.mod
@@ -2,4 +2,4 @@ module myapp
 
 go 1.23.1
 
-require github.com/nolssmit/Golang/GolangPackages/dog v0.0.0-20241101144710-9633512f64f4
+require github.com/nolssmit/Golang/GolangPackages/dog v0.0.0-20241102125419-58897132e2a3
