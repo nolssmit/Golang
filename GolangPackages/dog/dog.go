09300ac4d99e5eb1507e600babc0c4175395192b
Nols Smit <nols.smit@gmail.com>
2024-11-02 15:16:55 +0200

    WIP

--- GolangPackages/dog/dog.go
+++ GolangPackages/dog/dog.go
@@ -5,18 +5,18 @@ import (
 	"strings"
 )
 
-// WhenGrownUp returns the passed in string in uppercase.
+// WhenGrownUp returns the passed string in uppercase.
 func WhenGrownUp(s string) string {
 	return "When the puppy grows up it says: " + strings.ToUpper(s)
 }
 // Years returns the passed in number of years in dog years
-// where one human year is 10 dog years
+// where one human year is 10 dog years.
 func Years(y int) int {
 	return  y * 10
 }	
 
 // YearsTwo returns the passed in number of years in dog years
-// where one human year is 20 dog years
+// where one human year is 20 dog years.
 func YearsTwo(y int) int {
 	return  y * 20
 }	
